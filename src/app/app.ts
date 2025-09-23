bd9aa51fa1eb904dc7f8f2d40981988c7fb02bd1
Manzamichi <cmanzanero432@gmail.com>
2025-09-23 12:03:24 -0600

    Features del programa de transformación

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderModule } from '././header/header.module';
 import { Hero } from "./hero/hero/hero";
+import { Features } from "./features/features/features";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderModule, Hero],
+  imports: [RouterOutlet, HeaderModule, Hero, Features],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
