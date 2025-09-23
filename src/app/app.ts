844c7cda83f073f6f44b03d01e4a90898747c302
Manzamichi <cmanzanero432@gmail.com>
2025-09-23 12:25:08 -0600

    Revert "Features del programa de transformación"
    This reverts commit bd9aa51fa1eb904dc7f8f2d40981988c7fb02bd1.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderModule } from '././header/header.module';
 import { Hero } from "./hero/hero/hero";
-import { Features } from "./features/features/features";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderModule, Hero, Features],
+  imports: [RouterOutlet, HeaderModule, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
