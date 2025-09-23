90ac51943ad87a809ef19181ec38c1f53efbabea
Manzamichi <cmanzanero432@gmail.com>
2025-09-23 12:48:27 -0600

    no se que salio mal

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderModule } from '././header/header.module';
 import { Hero } from "./hero/hero/hero";
+//import { Features } from "./features/features/features";
+import { Description } from "./description/description/description";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderModule, Hero],
+  imports: [RouterOutlet, HeaderModule, Hero, Description],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
