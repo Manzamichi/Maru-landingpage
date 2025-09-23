bc77178103484246c801d17d94b7695ec9c30043
Manzamichi <cmanzanero432@gmail.com>
2025-09-23 12:48:27 -0600

    Configuracion de la descripcion del programa

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderModule } from '././header/header.module';
 import { Hero } from "./hero/hero/hero";
+<<<<<<< HEAD
+=======
+import { Features } from "./features/features/features";
+import { Description } from "./description/description/description";
+>>>>>>> f081a51 (Configuracion de la descripcion del programa)
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderModule, Hero],
+  imports: [RouterOutlet, HeaderModule, Hero, Features, Description],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
