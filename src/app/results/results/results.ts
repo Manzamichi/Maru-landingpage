57ba394826617f6378542e27fe268a13d2719808
Manzamichi <cmanzanero432@gmail.com>
2025-09-27 13:24:14 -0600

    results con tarjetas

--- src/app/results/results/results.ts
+++ src/app/results/results/results.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,10 +7,58 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ],
   templateUrl: './results.html',
-  styleUrl: './results.scss'
+  styleUrl: './results.scss',
+  standalone: true
 })
-export class Results {
+export class Results implements OnInit {
+  leftImage = '/images/after.png';
+  rightImage = '/images/placeholder.png';
+  sliderPosition: number = 300; // Posición inicial
+  containerWidth: number = 600;
+  isDragging: boolean = false;
+
+  ngOnInit(): void {
+    // Inicializar la posición del slider al centro del contenedor
+    this.sliderPosition = this.containerWidth / 2;
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (!this.isDragging) {
+      return; // Detiene la función si no se está arrastrando
+    }
+
+    const container = (event.currentTarget as HTMLElement).parentElement; // Apunta al contenedor principal
+    if (!container) return;
+
+    const rect = container.getBoundingClientRect();
+    let newPosition = event.clientX - rect.left;
+
+    // Lógica para limitar la posición
+    if (newPosition < 0) {
+      newPosition = 0;
+    } else if (newPosition > rect.width) {
+      newPosition = rect.width;
+    }
+
+    this.sliderPosition = newPosition;
+  }
+
+  // 1. Iniciar Arrastre: Cuando el usuario presiona el mouse sobre el deslizador
+  startDragging(event: MouseEvent) {
+    event.preventDefault(); // Evita que el navegador arrastre otros elementos
+    this.isDragging = true;
+  }
+
+  // 2. Detener Arrastre: Cuando el usuario suelta el mouse (en cualquier lugar)
+  stopDragging() {
+    this.isDragging = false;
+  }
+
   Results = [
+    { image: '/images/before-after.png', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", alt: "hola" },
+    { image: '/images/before.png', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", alt: "hola" },
+    { image: '/images/after.png', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", alt: "hola" },
+    { image: '/images/diet.jpg', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", alt: "hola" },
     { image: '/images/placeholder.png', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", alt: "hola" },
     { image: '/images/placeholder.png', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", alt: "hola" },
     { image: '/images/placeholder.png', description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", alt: "hola" },
