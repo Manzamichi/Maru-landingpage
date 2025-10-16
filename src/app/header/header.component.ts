73acda92a972b9550dae2015802f66883f44de5e
Manzamichi <cmanzanero432@gmail.com>
2025-10-16 15:53:11 -0600

    Agregar iconos al menu bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MenubarModule } from 'primeng/menubar';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Menubar, MenubarModule } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
 import { AvatarModule } from 'primeng/avatar';
 import { InputTextModule } from 'primeng/inputtext';
@@ -18,10 +18,12 @@ import { RouterModule } from '@angular/router';
     AvatarModule,
     InputTextModule,
     CommonModule,
-    RouterModule
+    RouterModule,
   ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('menuBar') menuBar: Menubar | undefined;
 
   constructor() { }
 
@@ -35,12 +37,29 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.items = [
-      { label: 'Home', command: () => this.scrollToSection('hero-section')},
-      { label: 'About', command: () => this.scrollToSection('about-section') },
-      { label: 'Services', command: () => this.scrollToSection('services-section') },
-      { label: 'Testimonials', command: () => this.scrollToSection('testimonials-section')},
-      { label: 'Blog', command: () => this.scrollToSection('blog-section') }
+      { label: 'Inicio', command: () => this.scrollToSection('hero-section'), icon: "pi pi-home" },
+      { label: 'Acerca de', command: () => this.scrollToSection('about-section'), icon: "pi pi-info-circle" },
+      { label: 'Servicios', command: () => this.scrollToSection('services-section'), icon: "pi pi-sparkles" },
+      { label: 'Resultados', command: () => this.scrollToSection('testimonials-section'), icon: "pi pi-user-edit"},
+      { label: 'Contactanos', command: () => this.scrollToSection('blog-section'), icon: "pi pi-address-book"}
     ];
+  }
+
+  ngAfterViewInit() {
+    this.setupOutsideClickListener();
+  }
+
+  setupOutsideClickListener() {
+    document.addEventListener('click', (event: MouseEvent) => {
+      const menuElement = this.menuBar?.el.nativeElement;
+      if (!menuElement) return;
+      const targetElement = event.target as HTMLElement;
+      if (targetElement.classList.contains('p-menubar-mobile-active')) {
+          if (this.menuBar) {
+              this.menuBar.mobileActive = false;
+          }
+      }
+    });
   }
 
 }
