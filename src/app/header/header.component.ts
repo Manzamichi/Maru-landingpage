11d83492f2613f8e0584058822f459f4f21258fa
Manzamichi <cmanzanero432@gmail.com>
2025-10-18 09:27:15 -0600

    Quité los bordes de toda la página y el menu ya es más responsivo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { Menubar, MenubarModule } from 'primeng/menubar';
 import { BadgeModule } from 'primeng/badge';
 import { AvatarModule } from 'primeng/avatar';
@@ -6,6 +6,12 @@ import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
 import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
+import { BreakpointObserver, Breakpoints, LayoutModule } from '@angular/cdk/layout';
+
+
+interface CustomMenuItem extends MenuItem {
+  mobileOnly?: boolean; 
+}
 
 @Component({
   selector: 'app-header',
@@ -19,15 +25,23 @@ import { RouterModule } from '@angular/router';
     InputTextModule,
     CommonModule,
     RouterModule,
-  ]
+    LayoutModule
+]
 })
 export class HeaderComponent implements OnInit, AfterViewInit {
 
   @ViewChild('menuBar') menuBar: Menubar | undefined;
 
-  constructor() { }
+  isMobile: boolean = false;
+  items: CustomMenuItem[] = [];
+  originalItems: CustomMenuItem[] = [];
 
-  items: MenuItem[] | undefined;
+  constructor(
+    private readonly breakpointObserver: BreakpointObserver,
+    private readonly cd: ChangeDetectorRef
+  ) { }
+
+  baseItems: MenuItem[] | undefined;
   scrollToSection(sectionId: string): void {
     const element = document.getElementById(sectionId);
     if (element) {
@@ -36,13 +50,40 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.items = [
+
+    const baseItems: CustomMenuItem[] = [
       { label: 'Inicio', command: () => this.scrollToSection('hero-section'), icon: "pi pi-home" },
       { label: 'Acerca de', command: () => this.scrollToSection('about-section'), icon: "pi pi-info-circle" },
       { label: 'Servicios', command: () => this.scrollToSection('services-section'), icon: "pi pi-sparkles" },
       { label: 'Resultados', command: () => this.scrollToSection('testimonials-section'), icon: "pi pi-user-edit"},
       { label: 'Contactanos', command: () => this.scrollToSection('blog-section'), icon: "pi pi-address-book"}
     ];
+
+    const mobileAppButton: CustomMenuItem = {
+      label: 'Nuestra app', command: () => window.location.href = 'https://mobile-maru.mastergeek.mx/login', icon: 'pi pi-mobile', mobileOnly: true
+    }
+
+    this.items = [...baseItems, mobileAppButton];
+
+    this.originalItems = [...this.items];
+
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      '(max-width: 960px)'
+    ]).subscribe(result => {
+      this.isMobile = result.matches;
+      this.filterMenuItems(); 
+      this.cd.detectChanges(); 
+    });
+  }
+
+  filterMenuItems() {
+    if (!this.isMobile) {
+      this.items = this.items.filter(item => !item.mobileOnly);
+    } else {
+      this.items = [...this.originalItems];
+    }
   }
 
   ngAfterViewInit() {
